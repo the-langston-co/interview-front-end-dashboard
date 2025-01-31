f34b36b3b631883f25b8e0cc04144afda291e45b
Neil Poulin <neil.j.poulin@gmail.com>
2025-01-31 14:57:07 -0700

    remove user print to customer page

--- app/(dashboard)/customers/page.tsx
+++ app/(dashboard)/customers/page.tsx
@@ -5,17 +5,15 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card';
-import { auth } from '@/lib/auth';
 
 export default async function CustomersPage() {
-  const session = await auth();
   return (
     <Card>
       <CardHeader>
         <CardTitle>Customers</CardTitle>
         <CardDescription>View all customers and their orders.</CardDescription>
       </CardHeader>
-      <CardContent>{JSON.stringify(session, null, 2)}</CardContent>
+      <CardContent></CardContent>
     </Card>
   );
 }
